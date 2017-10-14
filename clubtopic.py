a9d37570dd3097597c4e35f347a05e2fca2c2d91
Kunsi <hi@kunsmann.eu>
2017-10-14 10:59:28 +0200

    Initial commit: clubtopic.py
    This module checks the current opening state of the club, and updates the topic
    of the channel accordingly.

---
+++ clubtopic.py
@@ -0,0 +1,32 @@
+import sopel.module
+import requests
+import re
+
+@sopel.module.thread(True)
+@sopel.module.interval(15)
+def entropia_club_topic(bot):
+  headers = {'user-agent': 'Entropia IRC-Bot by Kunsi'}
+  channel = '#entropia'
+  url = 'http://club.entropia.de/status.json'
+
+  if channel in bot.channels:
+    r = requests.get(url)
+
+    if r.status_code == requests.codes.ok:
+      clubstatus = r.json()
+      old_topic = bot.channels[channel].topic
+
+#      bot.msg(channel, 'Clubstatus: ' + str(clubstatus['club_offen']))
+
+      if clubstatus['club_offen']:
+        status = 'offen'
+      else:
+        status = 'geschlossen'
+
+      new_topic = re.sub(r"(club[^:]*:) \w+", "\\1 " + status, old_topic, flags=re.IGNORECASE)
+
+      if old_topic != new_topic:
+        bot.write(('TOPIC', channel,), text=new_topic)
+
+#      bot.msg(channel, new_topic)
+
